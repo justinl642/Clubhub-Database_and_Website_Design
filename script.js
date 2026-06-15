7d02395d6da10e042a33eb4dacb8a860d1c576d0
justinl642 <justinl642@nycstudents.net>
2026-06-15 01:14:14 +0000

    e

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-// 1. Fetch the static JSON data file
+
 fetch("./restaurants.json")
     .then(response => response.json())
     .then(restaurants => {
         
-        // Find the container container in the HTML
+      
         const container = document.getElementById("restaurant-container");
         
-        // This variable will hold all of our card HTML blocks
+        
         let allCardsHTML = "";
 
-        // 2. Loop through each restaurant inside your list using a simple loop
+        
         for (let i = 0; i < restaurants.length; i++) {
             let shop = restaurants[i];
 
-            // Setup a simple if-statement to handle the Open/Closed text and styling
+            
             let statusText = "Closed";
             let statusClass = "closed";
             
@@ -22,7 +22,7 @@ fetch("./restaurants.json")
                 statusClass = "open";
             }
 
-            // 3 & 4. Build the HTML block for this specific card
+            
             allCardsHTML += `
                 <div class="restaurant-card">
                     <h3>${shop.name}</h3>
@@ -34,7 +34,7 @@ fetch("./restaurants.json")
             `;
         }
 
-        // 5. Inject all the generated cards directly into your container without using append
+       
         container.innerHTML = allCardsHTML;
 
     })
